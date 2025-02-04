d41c6bc80535223b05397c6fe034f8083d3f651c
kkanhar <karishmakanhar5@gmail.com>
2025-02-04 16:13:55 +0530

    Completed Assignment_1

---
+++ Week_3/Assignment_1/Assignment_1.js
@@ -0,0 +1,18 @@
+let str="Karishma"
+let num=123
+let boolean= true;
+let undefined;
+let obj={
+    name: "karishma",
+    salary: 2345,
+    isAdult: true
+
+
+}
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof boolean);
+console.log(typeof undefined);
+console.log(typeof obj);
+

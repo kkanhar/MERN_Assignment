0d5f7d240a079dba433886927828ba730c826fc6
kkanhar <karishmakanhar5@gmail.com>
2025-02-08 18:41:04 +0530

    Update Assignment_5.js

--- Week_3/Assignment_5.js
+++ Week_3/Assignment_5.js
@@ -2,5 +2,5 @@ let num = prompt("Enter a number:")
 
 for(let i = 1; i<= 10; i++){
   let result = num * i; 
-  console.log('${num} * ${i} = ${result}');
+  console.log(`${num} * ${i} = ${result}`);
 }

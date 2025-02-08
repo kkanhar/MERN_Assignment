eb23bb18db381a57e00d7f66c981d3ce550a48d7
kkanhar <karishmakanhar5@gmail.com>
2025-02-08 13:11:38 +0530

    completed Assignment_4.js

---
+++ Week_3/Assignment_1/Assignment_4.js
@@ -0,0 +1,18 @@
+let mark = prompt("Enter your mark:")
+
+if(mark >= 90)
+{
+  alert("Grade A")
+}
+else if(mark>= 75 && <90)
+{
+  alert("Grade B")
+}
+else if(mark>= 50 && <75)
+{
+  alert("Grade C")
+}
+else if(mark<50)
+{
+  alert("Fail")
+}

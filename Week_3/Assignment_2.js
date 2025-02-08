6ddaa0fa9a5967153dde60b25a6ee46b8dd39453
kkanhar <karishmakanhar5@gmail.com>
2025-02-08 12:45:07 +0530

    co

---
+++ Week_3/Assignment_2.js
@@ -0,0 +1,12 @@
+let num1 = prompt("Enter the first number:");
+let num2 = prompt("Enter the second number:");
+
+let add = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+
+alert("Addition: " + add + "\n" +
+      "Subtraction: " + sub + "\n" +
+      "Multiplication: " + mul + "\n" +
+      "Division: " + div);

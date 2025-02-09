8f4e663bd83f1dd5d0192f88c6402bc469e1d4f2
kkanhar <karishmakanhar5@gmail.com>
2025-02-10 00:48:32 +0530

    Completed Assignment_1.js

---
+++ Week_4_5/Assignment_1.js
@@ -0,0 +1,17 @@
+function start() {
+ console.log("Start");
+}
+
+function process() {
+  console.log("Processing...");
+}
+
+function end() {
+  console.log("End");
+}
+
+start();
+
+process();
+
+end();

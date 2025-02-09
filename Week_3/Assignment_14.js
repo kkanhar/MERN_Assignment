bfb50d40eb9423789df0ff703b1271af5fb6ae1e
kkanhar <karishmakanhar5@gmail.com>
2025-02-09 15:53:55 +0530

    Completed Assignment_14.js

---
+++ Week_3/Assignment_14.js
@@ -0,0 +1,12 @@
+let str = "JavaScript is fun "
+let word1 = str.split(" ")
+console.log(word1[0])
+let word2 = str.split(" ")
+console.log(word2[1])
+let word3 = str.split(" ")
+console.log(word3[2])
+
+let arr = []
+let addArr = arr.unshift("word1 ")
+console.log("Array:", arr )
+

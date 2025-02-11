1849d113dc761f0e9c5a49b7d24cd83086dc22cf
kkanhar <karishmakanhar5@gmail.com>
2025-02-12 01:31:29 +0530

    Completed Assignment_10.js

---
+++ Week_4_5/Assignment_10.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <= 10; i++) {
+  let output = "";
+
+  if (i % 3 === 0) {
+    output += "Fizz";
+  }
+
+  if (i % 5 === 0) {
+    output += "Buzz";
+  }
+
+  if (output === "") {
+    output = i;
+  }
+
+  console.log(output);
+  
+}
